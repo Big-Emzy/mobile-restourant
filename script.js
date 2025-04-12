7b732de8d20f8597bbe00764b844709f6fca080e
David Unah <davidunah022@gmail.com>
2025-04-13 00:49:50 +0100

    implementing the additem functionalities

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { itemsData } from "./data.js";
 
 const itemContainer = document.getElementById('itemsContainer')
-console.log(itemContainer)
+const itemsPicked = document.getElementById('itemsPickedContainer')
 
 let itemsBox = ''
 const items = itemsData.map(function(item) {
@@ -17,8 +17,8 @@ const items = itemsData.map(function(item) {
                         <p class="itemPrice">$${item.price}</p>
                     </div>
                 </div>
-                <div class="addItemsIcon" role="button">
-                    <img src="./public/images/addItemIcon.png" alt="adding button" class="add-icon">
+                <div class="addItemsIcon" role="button" data-add=${item.name}>
+                    <img src="./public/images/addItemIcon.png" alt="adding button" class="add-icon" data-add=${item.name}>
                 </div>
             </div>
     `
@@ -27,4 +27,50 @@ const items = itemsData.map(function(item) {
 
 itemContainer.innerHTML = itemsBox
 
-console.log(items)
+document.addEventListener('click',function(e) {
+        addItem(e.target)
+})
+
+
+//this is the function for adding items
+let listOfOrderedItems = []
+let selectedItems = ''
+function addItem(e) {
+ if (e.classList.contains('add-icon') || e.classList.contains('addItemsIcon')) {
+       // listOfOrderedItems.push(e.dataset.add)
+        itemsPicked.style.display = 'block'
+        selectedItems += `
+                     <h3 id="your-order">Your Order</h3>
+                            <div id="ordered-Items-Container">
+                                <div class="ordered-Items">
+                                    <div class="ordered-item-nameBox">
+                                        <h4>${e.dataset.add}</h4>
+                                        <p>remove</p>
+                                    </div>
+                                    <div class="order-item-price">
+                                        <p>$14</p>
+                                    </div>
+                                </div>
+                            </div>     
+`
+ }
+
+ itemsPicked.innerHTML = selectedItems
+ 
+} 
+
+
+`
+<!-- total ordered item section -->
+                            <div>
+                                <div class="total-order-box">
+                                    <div class="ordered-item-nameBox">
+                                        <h4>Total Price:</h4>
+                                    </div>
+                                    <div class="order-item-price">
+                                        <p>$14</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <button id="complete-order-btn" type="submit">Complete order</button>
+`
